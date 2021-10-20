b785be4ca1a153eaa7be45a3adc70a98eb280b27
Sonali Runwal <srunwal@ltu.edu>
2021-10-20 09:55:43 -0400

    Initial commit

---
+++ GithubAPI/GithubAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubAPI
+//
+//  Created by Sonali Runwal on 10/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
